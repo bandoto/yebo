fb88fae9722aa975be9c142d1a2670619a516dae
Vadim Percov <48429454+bandoto@users.noreply.github.com>
2020-07-21 01:27:00 +0300

    yebo
    Adaptive Flexbox site

---
+++ js/script.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+    $('.menu__burger').click(function(event) {
+        $('.menu__burger,.menu__body').toggleClass('active');
+    });
+});
+
+$(document).ready(function() {
+    $('.slider__body').slick({
+        dots: true,
+        arrows: false,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        adaptiveHeight: true
+    });
+});
+
+function ibg() {
+
+    $.each($('.ibg'), function(index, val) {
+        if ($(this).find('img').length > 0) {
+            $(this).css('background-image', 'url("' + $(this).find('img').attr('src') + '")');
+        }
+    });
+}
+
+ibg();
